533574dc7a718e5a7ae7da94fd0b154c7b1ce418
jap9 <jap9@alumnes.udl.cat>
2016-09-22 17:08:05 +0200

    First commit feature Send Private Message

---
+++ src/test/java/cat/udl/eps/softarch/steps/SendPrivateMessageStepDefs.java
@@ -0,0 +1,15 @@
+package cat.udl.eps.softarch.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by jap9 on 22/09/16.
+ */
+public class SendPrivateMessageStepDefs {
+    @When("^I send a private message with title \"([^\"]*)\"$")
+    public void iSendAPrivateMessageWithTitle(String title) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
