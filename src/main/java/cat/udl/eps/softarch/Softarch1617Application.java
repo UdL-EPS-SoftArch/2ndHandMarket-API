09ed0d9582ecd4adcfaedae2fbe77f177e2ee1ef
Roberto García <roberto@rhizomik.net>
2016-09-22 12:48:20 +0200

    Spring Boot 1.4.1 from initializr

---
+++ src/main/java/cat/udl/eps/softarch/Softarch1617Application.java
@@ -0,0 +1,12 @@
+package cat.udl.eps.softarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Softarch1617Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Softarch1617Application.class, args);
+	}
+}
---
+++ src/test/java/cat/udl/eps/softarch/Softarch1617ApplicationTests.java
@@ -0,0 +1,16 @@
+package cat.udl.eps.softarch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Softarch1617ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
