d5ef65ebf16df075b3225f2b5e3c0a145252b998
xavier <xplanes123@gmail.com>
2016-09-26 11:20:25 +0200

    Created class CounterOffer

---
+++ src/main/java/cat/udl/eps/softarch/domain/BuyerCounterOffer.java
@@ -0,0 +1,16 @@
+package cat.udl.eps.softarch.domain;
+
+/**
+ * Created by xavier on 26/09/16.
+ */
+public class BuyerCounterOffer extends CounterOffer {
+    @Override
+    public void setValue(float value){
+        if (value >= this.getValue()){
+            System.out.println("As a buyer, you want to set a lower price");
+        }
+        else{
+            super.setValue(value);
+        }
+    }
+}
