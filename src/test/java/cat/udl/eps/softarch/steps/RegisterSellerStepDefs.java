3f8858ba1072c1e04325de87aa2e85c5771a858a
Carles <carlessaez5@gmail.com>
2016-09-26 14:40:08 +0200

    Implementation of RegisterSeller

--- src/test/java/cat/udl/eps/softarch/steps/RegisterSellerStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/steps/RegisterSellerStepDefs.java
@@ -1,15 +1,65 @@
 package cat.udl.eps.softarch.steps;
 
+import cat.udl.eps.softarch.Softarch1617Application;
+import cat.udl.eps.softarch.domain.RegisterSeller;
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.ResultActions;
+import java.util.Random;
 
 /**
  * Created by Carles on 22/09/2016.
  */
+@ContextConfiguration(
+        classes = {Softarch1617Application.class}, loader = SpringBootContextLoader.class)
+@DirtiesContext
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+
 public class RegisterSellerStepDefs {
-    @When("^I create a seller with name \"([^\"]*)\"$")
-    public void iCreateASellerWithName(String arg0) throws Throwable {
+
+    private RegisterSeller seller;
+    private ResultActions result;
+
+    @When("^I create a seller with name \"([^\"]*)\" and mail \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void iCreateASellerWithNameAndMailAndPassword(String name, String mail, String password) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        seller = new RegisterSeller();
+        seller.setName(name);
+        seller.setMail(mail);
+        seller.setPassword(password);
     }
+    @Then("^An id is generated for this seller$")
+    public void anIdIsGeneratedForThisSeller() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        Random randomGenerator = new Random();
+        long randomId = randomGenerator.nextInt(100);
+        seller.setId(randomId);
+    }
+
+    @And("^There is a seller with name \"([^\"]*)\"$")
+    public void thereIsASellerWithName(String nameSeller) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        seller.getName().equals(nameSeller);
+    }
+
+    @And("^There is a seller with mail \"([^\"]*)\"$")
+    public void thereIsASellerWithMail(String mailSeller) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        seller.getMail().equals(mailSeller);    }
+
+    @And("^There is a seller with password \"([^\"]*)\"$")
+    public void andThereIsASellerWithPassword(String passwordSeller) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        seller.getPassword().equals(passwordSeller);
+    }
+
 }
--- src/test/resources/features/Register Seller.feature
+++ src/test/resources/features/Register Seller.feature
@@ -4,6 +4,8 @@ Feature: Post Advertisement
   I want to create a user with the seller propieties
 
   Scenario: Create seller
-    When I create a seller with name "Pepo"
-    Then The status is 201
+    When I create a seller with name "Pepo" and mail "pepo@pepo.com" and password "hellopepo"
+    Then An id is generated for this seller
     And There is a seller with name "Pepo"
+    And There is a seller with mail "pepo@pepo.com"
+    And There is a seller with password "hellopepo"
