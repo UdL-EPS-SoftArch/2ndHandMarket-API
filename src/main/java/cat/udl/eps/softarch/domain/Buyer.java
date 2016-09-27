fcb675c83f3c6ced523947224e775a655d30c8ed
jennifer <jsc14@alumnes.udl.cat>
2016-09-27 16:50:09 +0200

    Create Buyer

--- src/main/java/cat/udl/eps/softarch/domain/Buyer.java
+++ src/main/java/cat/udl/eps/softarch/domain/Buyer.java
@@ -14,11 +14,42 @@ import java.util.Set;
  * Created by jennifer on 27/09/16.
  */
 @Entity
-public class Buyer extends User{
+public class Buyer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Title cannot be blank")
+    private String name;
+
+    private String description;
+
+    private double price;
+
+    public void setName(String name) {
+        this.name= name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 
 
 }
