9f1b90c9e1c4f094e0fe5ef8fa526e93174e937b
xavier <xplanes123@gmail.com>
2016-09-27 16:52:01 +0200

    Created class Offer and steps

--- src/test/java/cat/udl/eps/softarch/steps/BuyerCounterOfferStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/steps/BuyerCounterOfferStepDefs.java
@@ -1,14 +1,10 @@
 package cat.udl.eps.softarch.steps;
 
 import cat.udl.eps.softarch.Softarch1617Application;
-import cat.udl.eps.softarch.domain.BuyerCounterOffer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cucumber.api.PendingException;
 import cucumber.api.java.Before;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,12 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Objects;
-
-import javax.validation.constraints.AssertFalse;
-import javax.validation.constraints.AssertTrue;
 
 /**
  * Created by xavier on 27/09/16.
@@ -56,26 +46,9 @@ public class BuyerCounterOfferStepDefs {
                 .build();
     }
 
-
-    private BuyerCounterOffer buyerOffer= new BuyerCounterOffer();
-    private BuyerCounterOffer buyerOffer2= new BuyerCounterOffer();
-
-
-    @When("^I post a new buyer counter offer with a lower price value (\\d+\\.\\d+)$")
-    public void iPostANewBuyerCounterOfferWithALowerPriceValue(float newValue) throws Throwable {
-        buyerOffer.setValue(110);
-        Assert.assertTrue(newValue < buyerOffer.getValue());
-    }
-
     @Then("^A counter-offer id has been generated$")
     public void aCounterOfferIdHasBeenGenerated() throws Throwable {
-        Assert.assertFalse(Objects.equals(buyerOffer, buyerOffer2));
-    }
-
-    @And("^There is a new buyer counter offer with a lower price value (\\d+\\.\\d+)$")
-    public void thereIsANewBuyerCounterOfferWithALowerPriceValue(float newValue) throws Throwable {
-        Assert.assertFalse(Objects.equals(buyerOffer, null));
-        buyerOffer2.setValue(newValue);
-        Assert.assertTrue(buyerOffer2.getValue() == newValue);
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
     }
 }
