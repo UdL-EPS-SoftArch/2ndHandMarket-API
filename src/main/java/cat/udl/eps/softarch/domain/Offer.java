0e3ae0df1d40a3d6d27bd00ce4043a5aba66cad3
xavier <xplanes123@gmail.com>
2016-09-27 16:52:01 +0200

    Created class Offer and steps

--- src/main/java/cat/udl/eps/softarch/domain/Offer.java
+++ src/main/java/cat/udl/eps/softarch/domain/Offer.java
@@ -2,10 +2,10 @@ package cat.udl.eps.softarch.domain;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import java.sql.Timestamp;
-import java.util.Date;
+import javax.persistence.Id;
 
 /**
  * Created by xavier on 22/09/16.
@@ -19,14 +19,7 @@ public class Offer {
     private Long id;
 
     @NotBlank(message = "The offer cannot be null")
-    private float value = 0;
-    //private Advertisement advert = new Advertisement();
-    private java.util.Date date= new java.util.Date();
-    private Timestamp time = new Timestamp(date.getTime());
-
-    public Long getId(){
-        return id;
-    }
+    private float value;
 
     public float getValue() {
         return value;
@@ -34,14 +27,5 @@ public class Offer {
 
     public void setValue(float value) {
         this.value = value;
-    }
-
-   /*public Advertisement getAdvertisement(){
-        return advert;
-    }*/
-
-    public Timestamp getData(){
-        System.out.println(time);
-        return time;
     }
 }
