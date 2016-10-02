dbb40ea6b6702ca11885763600637d1eff1443cf
xavier <xplanes123@gmail.com>
2016-10-02 12:00:37 +0200

    Buyer counter offer all tests passed

--- src/main/java/cat/udl/eps/softarch/domain/Offer.java
+++ src/main/java/cat/udl/eps/softarch/domain/Offer.java
@@ -2,10 +2,10 @@ package cat.udl.eps.softarch.domain;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import javax.persistence.*;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  * Created by xavier on 22/09/16.
@@ -20,6 +20,9 @@ public class Offer {
 
     @NotBlank(message = "The offer cannot be null")
     private float value = 0;
+    //private Advertisement advert = new Advertisement();
+    private java.util.Date date= new java.util.Date();
+    private Timestamp time = new Timestamp(date.getTime());
 
     public Long getId(){
         return id;
@@ -31,5 +34,14 @@ public class Offer {
 
     public void setValue(float value) {
         this.value = value;
+    }
+
+   /*public Advertisement getAdvertisement(){
+        return advert;
+    }*/
+
+    public Timestamp getData(){
+        System.out.println(time);
+        return time;
     }
 }
