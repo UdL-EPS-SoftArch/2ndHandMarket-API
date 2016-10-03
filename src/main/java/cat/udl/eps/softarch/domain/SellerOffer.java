972c8eac397a4c9af941478ab333537cca4f280c
ierathenz <julioqr94@gmail.com>
2016-10-03 18:36:06 +0200

    Firsts tests details, trying to be compatible with the other branch classes.

--- src/main/java/cat/udl/eps/softarch/domain/SellerOffer.java
+++ src/main/java/cat/udl/eps/softarch/domain/SellerOffer.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 /**
  * Created by julio on 22/09/16..
@@ -18,6 +19,11 @@ public class SellerOffer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long sellerId;
 
+    /*Time variable in order to know the time when
+    the offer was made*/
+    private Date date;
+
+    @NotBlank(message = "The offer cannot be null")
     private float value;
 
     public long getSellerId() {
@@ -30,5 +36,17 @@ public class SellerOffer {
 
     public void setValue(float value) {
             this.value = value;
+    }
+
+    public void setDate(Date date){
+        this.date = new Date();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void dateToString() {
+        System.out.println(date.toString());
     }
 }
---
+++ src/test/java/cat/udl/eps/softarch/steps/SellerCounterOfferStepDefs.java
@@ -0,0 +1,83 @@
+package cat.udl.eps.softarch.steps;
+
+import cat.udl.eps.softarch.Softarch1617Application;
+import cat.udl.eps.softarch.domain.SellerCounterOffer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cucumber.api.PendingException;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.AssertTrue;
+
+/**
+ * Created by ierathenz on 2/10/16.
+ */
+
+@ContextConfiguration(
+        classes = {Softarch1617Application.class}, loader = SpringBootContextLoader.class)
+@DirtiesContext
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+public class SellerCounterOfferStepDefs {
+    private static final Logger logger = LoggerFactory.getLogger(cat.udl.eps.softarch.steps.PostAdvertisementStepDefs.class);
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+    private ResultActions result;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(this.wac)
+                .build();
+    }
+
+    private SellerCounterOffer sellerOffer1 = new SellerCounterOffer();
+    private SellerCounterOffer sellerOffer2 = new SellerCounterOffer();
+
+    @When("^I change the offer value to (\\d+\\.\\d+)$")
+    public void iChangeTheOfferValueTo(float newValue) throws Throwable {
+        sellerOffer1.setValue(20.5f);
+        Assert.assertTrue(newValue > sellerOffer1.getValue());
+        //throw new PendingException();
+    }
+
+    @Then("^A counter-offer id has been generated$")
+    public void aCounterOfferIdHasBeenGenerated() throws Throwable {
+        Assert.assertFalse(Objects.equals(sellerOffer1, sellerOffer2));
+        //throw new PendingException();
+    }
+
+    @And("^There is a new seller counter offer with an upper price value (\\d+\\.\\d+)$")
+    public void thereIsANewSellerCounterOfferWithALowerPriceValue(float newValue) throws Throwable {
+        Assert.assertFalse(Objects.equals(sellerOffer1, null));
+        sellerOffer2.setValue(newValue);
+        Assert.assertTrue(sellerOffer2.getValue() == newValue);
+    }
+
+}
+
+
