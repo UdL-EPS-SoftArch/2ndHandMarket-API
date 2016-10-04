5061f4dd6c26dd922711b4ac4c1ef2ae97f4581f
Roberto García <roberto@rhizomik.net>
2016-10-04 11:03:56 +0200

    Added security configs and rest event handlers validation

---
+++ src/main/java/cat/udl/eps/softarch/config/RestValidationConfig.java
@@ -0,0 +1,27 @@
+package cat.udl.eps.softarch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * Created by http://rhizomik.net/~roberto/
+ */
+@Configuration
+public class RestValidationConfig extends RepositoryRestConfigurerAdapter {
+
+    @Bean
+    Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        Validator validator = validator();
+        validatingListener.addValidator("beforeCreate", validator);
+        validatingListener.addValidator("beforeSave", validator);
+    }
+}
