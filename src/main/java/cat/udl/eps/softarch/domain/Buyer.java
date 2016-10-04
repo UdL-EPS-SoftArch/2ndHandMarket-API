181ab9d910b6ec88dc38782c02ec32b26c70ed15
jennifer <jsc14@alumnes.udl.cat>
2016-10-04 09:52:30 +0200

    Test Buyer

--- src/main/java/cat/udl/eps/softarch/domain/Buyer.java
+++ src/main/java/cat/udl/eps/softarch/domain/Buyer.java
@@ -14,42 +14,11 @@ import java.util.Set;
  * Created by jennifer on 27/09/16.
  */
 @Entity
-public class Buyer {
+public class Buyer extends User{
 
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @NotBlank(message = "Title cannot be blank")
-    private String name;
-
-    private String description;
-
-    private double price;
-
-    public void setName(String name) {
-        this.name= name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
 
 
 }
