2e064f79f8e726884cba9297c41d6e55968d960d
zurfyx <zurfyx@gmail.com>
2016-10-08 21:16:00 +0200

    ad put/patch/delete handlers

--- src/main/java/cat/udl/eps/softarch/handler/AdvertisementEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/handler/AdvertisementEventHandler.java
@@ -6,12 +6,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import java.time.ZonedDateTime;
 
 @Component
 @RepositoryEventHandler(Advertisement.class)
 public class AdvertisementEventHandler {
+    private static final String NOT_OWNER = "You are not the owner";
 
     @HandleBeforeCreate
     @Transactional
@@ -23,5 +25,30 @@ public class AdvertisementEventHandler {
 
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         advertisement.setOwner(username);
+    }
+
+    @HandleBeforeSave
+    @Transactional
+    public void handleAdvertisementPreSave(Advertisement advertisement) {
+//        Assert.isTrue(isOwner(advertisement), NOT_OWNER); // TODO
+        ZonedDateTime now = ZonedDateTime.now();
+        advertisement.setModifiedAt(now);
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        advertisement.setOwner(username);
+    }
+
+    @HandleBeforeDelete
+    @Transactional
+    public void handleAdvertisementPreDelete(Advertisement advertisement) {
+//        Assert.isTrue(isOwner(advertisement), NOT_OWNER); // TODO
+    }
+
+    private String loggedInAs() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    private Boolean isOwner(Advertisement advertisement) {
+        return advertisement.getOwner().equals(loggedInAs());
     }
 }
