417872c2af53abc8ba0f71f8be52c66606727281
ierathenz <julioqr94@gmail.com>
2016-10-09 13:23:46 +0200

    New changes on SellerOffer and SellerCounterOffer in order to be compatible with the new version of Offer and BuyerCounterOffer. Tests were also modificated because of the new class changes.

--- src/main/java/cat/udl/eps/softarch/domain/SellerOffer.java
+++ src/main/java/cat/udl/eps/softarch/domain/SellerOffer.java
@@ -1,9 +1,11 @@
 package cat.udl.eps.softarch.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
 
 /**
  * Created by julio on 22/09/16..
@@ -16,7 +18,19 @@ public class SellerOffer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long sellerId;
 
+    @NotBlank(message = "The offer cannot be null")
     private float value;
+
+    /*Time variable in order to know the time when
+    the offer was made*/
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private ZonedDateTime publish_date;
+
+    /*Advertisement object in order add functionalities to tests*/
+    @JsonIdentityReference(alwaysAsId=true)
+    @ManyToOne
+    private Advertisement advert;
 
     public long getSellerId() {
         return sellerId;
@@ -28,5 +42,25 @@ public class SellerOffer {
 
     public void setValue(float value) {
             this.value = value;
+    }
+
+    public Advertisement getAdvertisement(){
+        return advert;
+    }
+
+    public void setAdvertisement(Advertisement advert){
+        this.advert = advert;
+    }
+
+    public void setDate(ZonedDateTime date){
+        this.publish_date = publish_date;
+    }
+
+    public ZonedDateTime getDate() {
+        return publish_date;
+    }
+
+    public void dateToString() {
+        System.out.println(publish_date.toString());
     }
 }
