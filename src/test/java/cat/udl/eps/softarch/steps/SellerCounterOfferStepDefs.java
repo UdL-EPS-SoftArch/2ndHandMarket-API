a06d630fd53525feae9cc690baed7a1d29311270
Roberto García <roberto@rhizomik.net>
2016-10-10 15:48:43 +0200

    Adjust Seller Counter-Offer feature to intended model

--- src/test/java/cat/udl/eps/softarch/steps/SellerCounterOfferStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/steps/SellerCounterOfferStepDefs.java
@@ -1,21 +1,25 @@
 package cat.udl.eps.softarch.steps;
 
 import cat.udl.eps.softarch.Softarch1617Application;
+import cat.udl.eps.softarch.domain.Advertisement;
 import cat.udl.eps.softarch.domain.BuyerCounterOffer;
 import cat.udl.eps.softarch.domain.SellerCounterOffer;
 import cat.udl.eps.softarch.domain.SellerOffer;
+import cat.udl.eps.softarch.repository.AdvertisementRepository;
+import cat.udl.eps.softarch.repository.BuyerCounterOfferRepository;
+import cat.udl.eps.softarch.repository.SellerOfferRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,17 +28,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Objects;
 
-import javax.validation.constraints.AssertFalse;
-import javax.validation.constraints.AssertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by ierathenz on 2/10/16.
  */
-
 
 @ContextConfiguration(
         classes = {Softarch1617Application.class}, loader = SpringBootContextLoader.class)
@@ -44,12 +47,18 @@ import javax.validation.constraints.AssertTrue;
 public class SellerCounterOfferStepDefs {
     private static final Logger logger = LoggerFactory.getLogger(cat.udl.eps.softarch.steps.PostAdvertisementStepDefs.class);
 
-    @Autowired
-    private WebApplicationContext wac;
+    @Autowired  private WebApplicationContext wac;
     private MockMvc mockMvc;
     private ResultActions result;
-
     private ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired AdvertisementRepository advertisementRepository;
+    @Autowired SellerOfferRepository sellerOfferRepository;
+    @Autowired BuyerCounterOfferRepository buyerCounterOfferRepository;
+
+    private Advertisement advertisement;
+    private SellerOffer sellerOffer;
+    private BuyerCounterOffer buyerCounterOffer;
 
     @Before
     public void setup() {
@@ -58,35 +67,54 @@ public class SellerCounterOfferStepDefs {
                 .build();
     }
 
-    private SellerCounterOffer sellerOffer1 = new SellerCounterOffer();
-    private SellerCounterOffer sellerOffer2 = new SellerCounterOffer();
-    private String agent = new String("agent");
-    private BuyerCounterOffer buyerOffer= new BuyerCounterOffer();
-
-    @When("^I change the offer value to (\\d+\\.\\d+)$")
-    public void iChangeTheOfferValueTo(float newValue) throws Throwable {
-        sellerOffer1.setValue(20.5f);
-        sellerOffer1.setAgent(agent);
-        sellerOffer1.setRespondsTo(buyerOffer);
-        Assert.assertTrue(newValue > sellerOffer1.getValue());
-        //throw new PendingException();
+    @Given("^There is an existing advertisement with title \"([^\"]*)\"$")
+    public void thereIsAnAdvertisementWithTitle(String title) throws Throwable {
+        advertisement = new Advertisement();
+        advertisement.setTitle(title);
+        advertisement = advertisementRepository.save(advertisement);
     }
 
-    @Then("^A counter-offer id has been generated$")
-    public void aCounterOfferIdHasBeenGenerated() throws Throwable {
-        Assert.assertFalse(Objects.equals(sellerOffer1, sellerOffer2));
-        //throw new PendingException();
+    @And("^There is a seller offer for the previous advertisement with price value (\\d+\\.\\d+)$")
+    public void thereIsASellerOfferForThePreviousAdvertisementWithPriceValue(float value) throws Throwable {
+        sellerOffer = new SellerOffer();
+        sellerOffer.setAdvertisement(advertisement);
+        sellerOffer.setValue(value);
+        sellerOffer = sellerOfferRepository.save(sellerOffer);
     }
 
-    @And("^There is a new seller counter offer with an upper price value (\\d+\\.\\d+)$")
-    public void thereIsANewSellerCounterOfferWithALowerPriceValue(float newValue) throws Throwable {
-        Assert.assertFalse(Objects.equals(sellerOffer1, null));
-        sellerOffer2.setValue(newValue);
-        Assert.assertTrue(sellerOffer2.getValue() == newValue);
-        Assert.assertTrue(sellerOffer1.getAgent().equals(agent));
-        Assert.assertTrue(sellerOffer1.getRespondsTo().equals(buyerOffer));
+    @And("^There is a buyer counter offer for the previous seller offer with price value (\\d+\\.\\d+)$")
+    public void thereIsABuyerCounterOfferForThePreviousSellerOfferWithPriceValue(float value) throws Throwable {
+        buyerCounterOffer = new BuyerCounterOffer();
+        buyerCounterOffer.setRespondsTo(sellerOffer);
+        buyerCounterOffer.setValue(value);
+        buyerCounterOffer = buyerCounterOfferRepository.save(buyerCounterOffer);
     }
 
+    @When("^I create a seller counter offer for the previous buyer counter offer with price value (\\d+\\.\\d+)$")
+    public void iCreateASellerCounterOfferForThePreviousBuyerCounterOfferWithPriceValue(float value) throws Throwable {
+        SellerCounterOffer sellerCounterOffer = new SellerCounterOffer();
+        sellerCounterOffer.setRespondsTo(buyerCounterOffer);
+        sellerCounterOffer.setValue(value);
+
+        String message = mapper.writeValueAsString(sellerCounterOffer);
+
+        result = mockMvc.perform(
+                post("/sellerCounterOffers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(message)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @Then("^There is a new seller counter offer with price value (\\d+\\.\\d+)$")
+    public void thereIsANewSellerCounterOfferWithPriceValue(float value) throws Throwable {
+        String location = result.andReturn().getResponse().getHeader("Location");
+
+        mockMvc.perform(get(location)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.value").value(value));
+    }
 }
 
 
