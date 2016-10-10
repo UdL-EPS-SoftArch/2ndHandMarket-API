58bca3d8abda77444cfb16f3179e34d538961e10
srblimp <srblimp@gmail.com>
2016-10-10 15:10:06 +0200

    Updated features and StepDefs , all test OK

--- src/test/java/cat/udl/eps/softarch/steps/SearchAdvertisementStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/steps/SearchAdvertisementStepDefs.java
@@ -1,13 +1,18 @@
 package cat.udl.eps.softarch.steps;
 
 import cat.udl.eps.softarch.domain.Advertisement;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
+import cat.udl.eps.softarch.repository.AdvertisementRepository;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
@@ -16,18 +21,28 @@ import static org.junit.Assert.assertTrue;
  */
 public class SearchAdvertisementStepDefs {
 
+    @Autowired private AdvertisementRepository advertisementRepository;
+
     private Advertisement advert = new Advertisement();
 
-    @When("^I search an advertisement with the keyWord \"([^\"]*)\"")
-    public void SearchAdvertisement(String keyword) throws Throwable{
+    @Given("^There is an advertisement with the tag \"([^\"]*)\"$")
+    public void SearchAdvertisement(String tag) throws Throwable{
+        //Advertisement advert = new Advertisement();
         Set<String> tags = new HashSet<>();
-        tags.add(keyword);
+        tags.add(tag);
         advert.setTags(tags);
-
+        advert.setTitle(tag + "new");
+        advert = advertisementRepository.save(advert);
     }
 
-    @Then("^There is an advertisement with keyWord \"([^\"]*)\"$")
-    public void thereIsAnAdvertisementWithKeyWord(String keyWord) throws Throwable {
-        assertTrue(CollectionUtils.containsInstance(advert.getTags(), keyWord));
+    @Then("^I get an advertisement with tag \"([^\"]*)\"$")
+    public void thereIsAnAdvertisementWithKeyWord(String tag) throws Throwable {
+        assertTrue(CollectionUtils.containsInstance(advert.getTags(), tag));
     }
+
+    @Then("^There is not an advertisement with tag \"([^\"]*)\"$")
+    public void thereIsnTAnAdvertisementWithTag(String tag) throws Throwable {
+        assertFalse(CollectionUtils.containsInstance(advert.getTags(), tag));
+    }
+
 }
