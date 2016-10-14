95b528789d04057cdbdebb6d29a270a11e7b892c
Roberto García <roberto@rhizomik.net>
2016-10-14 15:17:01 +0200

    User table name to avoid collision with Heroku Postgres

--- src/main/java/cat/udl/eps/softarch/domain/User.java
+++ src/main/java/cat/udl/eps/softarch/domain/User.java
@@ -1,6 +1,5 @@
 package cat.udl.eps.softarch.domain;
 
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -9,8 +8,8 @@ import java.util.Date;
 
 
 @Entity
+@Table(name = "SoftArchUser") //Avoid collision with system table User in Postgres
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
