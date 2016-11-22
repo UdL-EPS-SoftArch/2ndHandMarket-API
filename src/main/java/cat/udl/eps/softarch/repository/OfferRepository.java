75456c9e88ded30d61da860e321cf6c5ce718f51
xavier <xplanes123@gmail.com>
2016-11-22 16:11:44 +0100

    Offer update to BuyerOffer refactor

--- src/main/java/cat/udl/eps/softarch/repository/OfferRepository.java
+++ src/main/java/cat/udl/eps/softarch/repository/OfferRepository.java
@@ -1,6 +1,6 @@
 package cat.udl.eps.softarch.repository;
 
-import cat.udl.eps.softarch.domain.Offer;
+import cat.udl.eps.softarch.domain.BuyerOffer;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -8,5 +8,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * Created by http://rhizomik.net/~roberto/
  */
 @RepositoryRestResource
-public interface OfferRepository extends PagingAndSortingRepository<Offer, Long> {
+public interface OfferRepository extends PagingAndSortingRepository<BuyerOffer, Long> {
 }
