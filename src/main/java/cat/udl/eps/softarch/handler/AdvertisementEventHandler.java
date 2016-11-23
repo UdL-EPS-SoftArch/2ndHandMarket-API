55454f739bf68609f52bbcac0f618cda12e2c349
Roberto García <roberto@rhizomik.net>
2016-11-23 15:56:45 +0100

    AdvertisementEventHandler before save sets modifiedAt

--- src/main/java/cat/udl/eps/softarch/handler/AdvertisementEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/handler/AdvertisementEventHandler.java
@@ -3,7 +3,10 @@ package cat.udl.eps.softarch.handler;
 import cat.udl.eps.softarch.domain.Advertisement;
 import cat.udl.eps.softarch.repository.AdvertisementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.core.annotation.*;
+import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeDelete;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -32,11 +35,7 @@ public class AdvertisementEventHandler {
     @Transactional
     @PreAuthorize("#advertisement.owner == authentication.name")
     public void handleAdvertisementPreSave(Advertisement advertisement) {
-        // Maintain createdAt and owner
-        Advertisement oldAdvertisement = advertisementRepository.findOne(advertisement.getId());
-        advertisement.setOwner(oldAdvertisement.getOwner());
-        advertisement.setCreatedAt(advertisement.getCreatedAt());
-
+        // Note: owner and createdAt cannot be changed through API because setters are @JsonIgnore
         ZonedDateTime now = ZonedDateTime.now();
         advertisement.setModifiedAt(now);
     }
