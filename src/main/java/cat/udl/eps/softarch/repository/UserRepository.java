22ec0b2acc5c1935599bc33c3f20793b6058ce8d
zurfyx <zurfyx@gmail.com>
2016-11-23 20:49:05 +0100

    Fix #34

--- src/main/java/cat/udl/eps/softarch/repository/UserRepository.java
+++ src/main/java/cat/udl/eps/softarch/repository/UserRepository.java
@@ -2,6 +2,7 @@ package cat.udl.eps.softarch.repository;
 
 import cat.udl.eps.softarch.domain.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
  */
 @RepositoryRestResource
 public interface UserRepository extends PagingAndSortingRepository<User,String>{
-    List<User> findByName(String name);
+    List<User> findByName(@Param("name") String name);
 }
