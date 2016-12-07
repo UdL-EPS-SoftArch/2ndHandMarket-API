522a4a892ba42412191af8e2c68110ecaef012b6
carles <carlessaez5@gmail.com>
2016-12-07 12:07:42 +0100

    solved some errors

--- src/main/java/cat/udl/eps/softarch/repository/BasketProductRepository.java
+++ src/main/java/cat/udl/eps/softarch/repository/BasketProductRepository.java
@@ -1,6 +1,6 @@
 package cat.udl.eps.softarch.repository;
 
-import cat.udl.eps.softarch.domain.PrivateMessage;
+import cat.udl.eps.softarch.domain.BasketProduct;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -8,5 +8,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * Created by carles on 29/11/16.
  */
 @RepositoryRestResource
-public interface BasketProductRepository  extends PagingAndSortingRepository<PrivateMessage, Long> {
+public interface BasketProductRepository  extends PagingAndSortingRepository<BasketProduct, Long> {
 }
