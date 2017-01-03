38370da4b08743f83661b1ef9b893c4b4ba7b1aa
Roberto García <roberto@rhizomik.net>
2017-01-03 14:35:10 +0100

    Security config for OAuth2 and keep Basic for testing

--- src/main/java/cat/udl/eps/softarch/config/WebSecurityConfig.java
+++ src/main/java/cat/udl/eps/softarch/config/WebSecurityConfig.java
@@ -1,12 +1,15 @@
 package cat.udl.eps.softarch.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 /**
@@ -15,27 +18,35 @@ import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Profile("!Test")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-            .authorizeRequests()
-                .antMatchers(HttpMethod.PUT, "/advertisements*/*").authenticated()
-                .antMatchers(HttpMethod.POST, "/advertisements*/*").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/advertisements*/*").authenticated()
-                .antMatchers(HttpMethod.PATCH, "/advertisements*/*").authenticated()
-                .antMatchers(HttpMethod.PUT, "/pictures*/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/pictures*/**").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/pictures*/**").authenticated()
-                .antMatchers(HttpMethod.PATCH, "/pictures*/**").authenticated()
-                .anyRequest().permitAll()
-                .and()
-            .httpBasic().realmName("SoftArch 2016-17")
-                .and()
+            .and()
             .addFilterBefore(new CORSFilter(), ChannelProcessingFilter.class)
             .csrf().disable();
+    }
+
+    @Configuration
+    @EnableResourceServer
+    @Profile("!Test")
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.PUT, "/advertisements*/*").authenticated()
+                    .antMatchers(HttpMethod.POST, "/advertisements*/*").authenticated()
+                    .antMatchers(HttpMethod.DELETE, "/advertisements*/*").authenticated()
+                    .antMatchers(HttpMethod.PATCH, "/advertisements*/*").authenticated()
+                    .antMatchers(HttpMethod.PUT, "/pictures*/**").authenticated()
+                    .antMatchers(HttpMethod.POST, "/pictures*/**").authenticated()
+                    .antMatchers(HttpMethod.DELETE, "/pictures*/**").authenticated()
+                    .antMatchers(HttpMethod.PATCH, "/pictures*/**").authenticated()
+                    .anyRequest().permitAll();
+        }
     }
 }
